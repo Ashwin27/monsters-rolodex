cbb81f53e127502f7f889402997a43a3663990f0
Ashwin Raman <ashwin.raman@instacart.com>
2021-05-09 22:53:18 -0400

    Hardcoding monsters to prevent insecure access error

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
+import MONSTERS_DATA from './monsters.data'
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [],
+      monsters: MONSTERS_DATA,
       search: ""
     };
   };
 
+  /*
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
-  }
+  }*/
 
   render() {
     const {monsters, search} = this.state;
